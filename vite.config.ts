1d45427a33789ed6bb6be4b62ae2f88669e1ab08
Indar Basto <indar.basto@gmail.com>
2023-07-20 19:13:23 +0300

    fix: ESLint config

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
       '@components': resolve(__dirname, './src/components'),
       '@layouts': resolve(__dirname, './src/layouts'),
       '@pages': resolve(__dirname, './src/pages'),
+      '@api': resolve(__dirname, './src/api'),
+      '@interfaces': resolve(__dirname, './src/interfaces'),
+      '@store': resolve(__dirname, './src/store'),
     },
   },
 });
