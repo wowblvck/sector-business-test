abd2e6f5be1cd4cee7d7845123b99a1ce76dde4b
Indar Basto <indar.basto@gmail.com>
2023-07-23 18:37:14 +0300

    feat: add search handle

--- src/api/endpoints/postsApi.ts
+++ src/api/endpoints/postsApi.ts
@@ -3,11 +3,10 @@ import { api } from '@api/api';
 
 const postsApi = api.injectEndpoints({
   endpoints: (build) => ({
-    getPostsList: build.query<Posts[], void>({
-      query: () => 'posts',
+    getPostsList: build.query<Posts[], string>({
+      query: (query: string) => `posts${query.length ? `?q=${query}` : ''}`,
     }),
   }),
-  overrideExisting: false,
 });
 
 export const { useGetPostsListQuery } = postsApi;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from '@api/api';
+import postsReducer from '@/reducers/posts.reducer';
 
 export const store = configureStore({
   reducer: {
+    postsReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
