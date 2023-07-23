067f2382b8e6181adeeffde02e2e8a7d86fd7fe4
Indar Basto <indar.basto@gmail.com>
2023-07-23 16:43:36 +0300

    feat: add posts sort

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -7,15 +7,18 @@ const columns: ColumnsType<Posts> = [
     title: 'ID',
     dataIndex: 'id',
     width: 25,
+    sorter: (a, b) => a.id - b.id,
   },
   {
     title: 'Заголовок',
     dataIndex: 'title',
     width: 200,
+    sorter: (a, b) => a.title.length - b.title.length,
   },
   {
     title: 'Описание',
     dataIndex: 'body',
+    sorter: (a, b) => a.body.length - b.body.length,
   },
 ];
 
@@ -33,10 +36,14 @@ const PostsList: React.FC<PostsListsProps> = ({ posts, loading }) => {
       bordered
       rowKey={(record: Posts) => record.id}
       pagination={false}
+      style={{ width: '100%' }}
       locale={{
         emptyText: (
           <Empty description="Нет загруженных постов" image={Empty.PRESENTED_IMAGE_SIMPLE} />
         ),
+        triggerAsc: 'Сортировка по возрастанию',
+        triggerDesc: 'Сортировка по убыванию',
+        cancelSort: 'Отменить сортировку',
       }}
     />
   );
